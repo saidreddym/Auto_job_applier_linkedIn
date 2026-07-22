6032891b860834b4ef3c03a34ce107bb758d09e8
unknown <saidireddym@gmail.com>
2026-07-22 17:34:34 +0000

    dd

--- config/secrets.py
+++ config/secrets.py
@@ -2,8 +2,8 @@
 
 
 # Login Credentials for LinkedIn (Optional)
-username = "saidireddym@gmail.com"       # Enter your username in the quotes
-password = "Quest@20533"           # Enter your password in the quotes
+username = "rrr@gmail.com"       # Enter your username in the quotes
+password = "ttt@20533"           # Enter your password in the quotes
 
 
 ## Artificial Intelligence (Beta Not-Recommended)
