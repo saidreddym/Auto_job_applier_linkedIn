9bf549ef5a65021f6154c79030f15dd4177d36d2
unknown <saidireddym@gmail.com>
2026-07-22 17:12:15 +0000

    ss

--- config/search.py
+++ config/search.py
@@ -74,7 +74,7 @@ about_company_bad_words = ["DeepLight AI", "Avrioc Technologies LLC", "Hire Feed
 about_company_good_words = []      # (dynamic multiple search) or leave empty as []. Ex: ["Robert Half", "Dice"]
 
 # Avoid applying to these companies if they have these bad words in their 'Job Description' section...  (In development)
-bad_words = ["US Citizen","USA Citizen","No C2C", "No Corp2Corp", ".NET", "Embedded Programming", "PHP", "Ruby", "CNC", "GCP", "Alibaba", "Oracle", "XMCloud", "Emirati"]                     # (dynamic multiple search) or leave empty as []. Case Insensitive. Ex: ["word_1", "phrase 1", "word word", "polygraph", "US Citizenship", "Security Clearance"]
+bad_words = ["US Citizen","USA Citizen","No C2C", "Detection", "No Corp2Corp", ".NET", "Embedded Programming", "PHP", "Ruby", "CNC", "GCP", "Alibaba", "Oracle", "XMCloud", "Emirati"]                     # (dynamic multiple search) or leave empty as []. Case Insensitive. Ex: ["word_1", "phrase 1", "word word", "polygraph", "US Citizenship", "Security Clearance"]
 
 # Do you have an active Security Clearance? (True for Yes and False for No)
 security_clearance = False         # True or False, Note: True or False are case-sensitive
